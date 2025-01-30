7878c85cc23770704abe5bd64344d69b179f0743
Afanasiev Alexander <jei12061988@yandex.ru>
2025-01-30 18:25:10 +0500

    fix details

--- build/script/script.js
+++ build/script/script.js
@@ -224,11 +224,18 @@ class ScrollTabs {
     this.buttons = tabsContainer.querySelectorAll('.js-tab-btn');
 
     this.keyAttributeName = 'data-key';
+    this.activeTabModifierClassName = 'tabs__btn_active';
 
     this.init();
   }
 
   scrollToTabContent(tab) {
+    this.buttons.forEach((item) => {
+      item.classList.remove(this.activeTabModifierClassName);
+    })
+
+    tab.classList.add(this.activeTabModifierClassName);
+
     const key = tab.getAttribute(this.keyAttributeName);
 
     const content = document.querySelector(`#${key}`)
